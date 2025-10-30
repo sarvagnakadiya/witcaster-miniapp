44f13d5a997811f5cdcebd9f461481dce8ce898c
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2025-10-30 11:48:10 +0530

    cors added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // Apply COOP headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "credentialless",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
